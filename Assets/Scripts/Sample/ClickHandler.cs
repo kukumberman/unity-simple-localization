7a8146ebdcd60857f176a9352a5929256d20d8e0
Dima <strongbaby1337@gmail.com>
2022-02-07 21:05:52 +0200

    demo showcase scripts

---
+++ Assets/Scripts/Sample/ClickHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour
+{
+    public event Action<ClickHandler> OnClick = null;
+
+    private int m_TimesClicked = 0;
+
+    public int TimesClicked => m_TimesClicked;
+
+    public void Click()
+    {
+        m_TimesClicked += 1;
+
+        OnClick?.Invoke(this);
+    }
+}
---
+++ Assets/Scripts/Sample/ScoreLabel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreLabel : LocalizedLabelTMPro
+{
+    [SerializeField] private ClickHandler m_ClickHandler = null;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        m_ClickHandler.OnClick += OnClick;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+
+        m_ClickHandler.OnClick -= OnClick;
+    }
+
+    protected override void UpdateLabel()
+    {
+        // base is being overwritten
+
+        m_Localization.TryFormatValue(m_Key, out string text, m_ClickHandler.TimesClicked);
+
+        m_Label.text = text;
+    }
+
+    private void OnClick(ClickHandler sender)
+    {
+        UpdateLabel();
+    }
+}
